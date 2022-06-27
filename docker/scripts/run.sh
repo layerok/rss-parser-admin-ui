eb43afa1af29d6cc5ca0fcd864e07e4732f4ce82
Vladimir Rudomanenko <vladimir.rudomanenko@rock-west.com>
2022-06-27 04:07:22 +0200

    update docker/scripts/run.sh

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
 cd /var/www
-
 composer install --optimize-autoloader --no-dev --ignore-platform-reqs
-
 php artisan cache:clear
 php artisan config:clear
 php artisan view:clear
 php artisan route:clear
-
 php artisan key:generate --force
 php artisan jwt:secret --force
 php artisan migrate:fresh --seed
 php artisan lifehacker:parse-rss
-
-
 /usr/bin/supervisord -c /etc/supervisord.conf
